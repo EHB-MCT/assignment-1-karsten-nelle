446fa8ce892e05e031241584b700147069ba0d07
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-10-11 11:34:50 +0200

    Add Footer and Nav  template components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// React compponents need to start with a capital letter
+
+import React from 'react'; // React components need to be imported
+
+
+// Footer component
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2021. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer; // Export the component so it can be used in other files (like App.js)
